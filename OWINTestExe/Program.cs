632fda7c7b5fca4216cacc2f30a7d6c6549e3092
Nicolas Blanco Skrlec <nicolas.blanco@col.vueling.com>
2019-01-04 14:24:28 +0100

    pruebas que no funcionan con EXE y gitignore

---
+++ OWINTestExe/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OWINTest.Exe
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            string baseAddress = "http://localhost:9000/";
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Thread.Sleep(Timeout.Infinite);
+
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ OWINTestExe/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Owin;
+
+namespace OWINTest.Exe
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+
+            appBuilder.Run(async context =>
+            {
+                await context.Response.WriteAsync("The api is exposed in /api/values");
+            });
+        }
+    }
+}
